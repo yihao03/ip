692707f9e4823b0e06ecd7e1d101c22bdab19cd5
yihao <yihao039@gmail.com>
2025-08-27 11:25:04 +0800

    chore: refactor Chat to its own class

--- src/main/java/Dumpy.java
+++ src/main/java/Dumpy.java
@@ -1,14 +1,9 @@
 import components.Todo;
-import components.task.DeadlineTask;
-import components.task.EventTask;
-import components.task.Task;
-import exceptions.TaskNotFoundException;
 import utilities.Data;
-import utilities.IO;
 
 public class Dumpy {
-    private static final String LINE_SEPARATOR = "----------------------------------------------------------------------";
-    private static final String LOGO = """
+    public static final String LINE_SEPARATOR = "----------------------------------------------------------------------";
+    public static final String LOGO = """
                                     ██████╗ ██╗   ██╗███╗   ███╗██████╗ ██╗   ██╗
                                     ██╔══██╗██║   ██║████╗ ████║██╔══██╗╚██╗ ██╔╝
                                     ██║  ██║██║   ██║██╔████╔██║██████╔╝ ╚████╔╝
@@ -21,54 +16,8 @@ public class Dumpy {
         // Initialization
         Todo todo = Data.readListFromFile();
 
-        // Greeting
-        System.out.printf("Hello, I am the smartest chatbot\n" + "%s\n" + "What can I do for you today?\n", Dumpy.LOGO);
+        Chat.start(todo);
 
-        while (true) {
-            System.out.println(Dumpy.LINE_SEPARATOR);
-            String input = IO.readLine();
-            System.out.println(Dumpy.LINE_SEPARATOR);
-
-            if (input.startsWith("mark")) {
-                int taskNumber = Integer.parseInt(input.replaceAll("[^0-9-]", ""));
-                try {
-                    Task task = todo.toggleDone(taskNumber);
-                    System.out.println(task.toString());
-                } catch (TaskNotFoundException e) {
-                    System.out.println(e.getMessage());
-                }
-            } else if (input.startsWith("delete")) {
-                int taskNumber = Integer.parseInt(input.replaceAll("[^0-9-]", ""));
-                try {
-                    todo.deleteTask(taskNumber);
-                } catch (TaskNotFoundException e) {
-                    System.out.println(e.getMessage());
-                }
-            } else {
-                switch (input) {
-                case "todo":
-                    Task task = Task.createTask();
-                    todo.addTask(task);
-                    break;
-                case "deadline":
-                    task = DeadlineTask.createTask();
-                    todo.addTask(task);
-                    break;
-                case "event":
-                    task = EventTask.createTask();
-                    todo.addTask(task);
-                    break;
-                case "list":
-                    System.out.print(todo.listTasks());
-                    break;
-                case "exit":
-                    System.out.println("Goodbye!");
-                    return;
-                default:
-                    System.out.println("Sorry I don't understand :(");
-                    break;
-                }
-            }
-        }
+        Data.saveListToFile(todo);
     }
 }
